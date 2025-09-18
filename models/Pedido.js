9760cbced9ac0b7f41af15d41a7593bc27e33119
Ángel Bautista Gutiérrez Ibarra <angelgutierrez@solemti.com>
2025-09-18 12:26:53 -0700

    cambios

---
+++ models/Pedido.js
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database.js";
+
+const Pedido = sequelize.define("Pedido", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  order_id: { type: DataTypes.BIGINT, allowNull: false },
+  email: { type: DataTypes.STRING },
+  total_price: { type: DataTypes.DECIMAL(10, 2) },
+  currency: { type: DataTypes.STRING },
+  created_at: { type: DataTypes.DATE },
+  data: { type: DataTypes.JSON },
+});
+
+
+export default Pedido;
